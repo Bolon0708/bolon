28116651b61eb12d38e571a9df796ccd681eae27
liangbl <17610828158@163.com>
2019-11-19 21:12:17 +0800

    first commit

---
+++ src/main/java/com/liang/bolon/BolonApplication.java
@@ -0,0 +1,13 @@
+package com.liang.bolon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BolonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BolonApplication.class, args);
+    }
+
+}
