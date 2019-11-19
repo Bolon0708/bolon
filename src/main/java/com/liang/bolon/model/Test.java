7e9dea72e375b99a6a613f588d382cf08159e5a7
liangbl <17610828158@163.com>
2019-11-19 21:49:55 +0800

    新增Test类

---
+++ src/main/java/com/liang/bolon/model/Test.java
@@ -0,0 +1,13 @@
+package com.liang.bolon.model;
+
+import lombok.Data;
+
+/**
+ * @ClassName: Test
+ * @Author: Bolon
+ * @Date: 2019/11/19 21:25
+ * @Description: 测试类
+ */
+@Data
+public class Test {
+}
